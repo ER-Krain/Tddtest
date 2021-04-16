4ff93b924ede5904d64c0ac09d4b8151761a644b
ER_Krain <1965287334@qq.com>
2021-04-16 13:17:01 +0800

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,7 @@
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):  #(1)
     
@@ -16,7 +18,34 @@ class NewVisitorTest(unittest.TestCase):  #(1)
 
         #She notice the page title and header mention to-do lists
         self.assertIn('To-Do',self.browser.title)  #(4)
-        self.fail('Finish the test!')  #(5)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)  #(5)
+
+        #She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+        
+        #She types "Buy peacock feathers" into a text box (Edith's hobby
+        #is trying fly-fishing luers)
+        inputbox.send_keys('Buy peacock feather')
+
+        #when she hits enter, the page updates, and now the page lists
+        #"1:Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tage_name('tr')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers' for row in rows)
+            )
+
+        #There is still a text box inviting her to add another item. She
+        #enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.fail('Finish the test')
 
 if __name__ == '__main__':  #(6)
     unittest.main(warnings='ignore')  #(7)
@@ -25,16 +54,13 @@ if __name__ == '__main__':  #(6)
 
 
 
-#She is invited to enter a to-do item straight away
 
-#She types "Buy peacock feathers" into a text box (Edith's hobby
-#is trying fly-fishing luers)
 
-#when she hits enter, the page updates, and now the page lists
-#"1:Buy peacock feathers" as an item in a to-do list
 
-#There is still a text box inviting her to add another item. She
-#enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+
+
+
 
 #The page updates again, and now shows both items on her list
 
