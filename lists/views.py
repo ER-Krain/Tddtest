04f32692d49dc1d680816097a6558874de86059e
ER_Krain <1965287334@qq.com>
2021-05-21 14:00:18 +0800

    new URL, view and templates to display lists

--- lists/views.py
+++ lists/views.py
@@ -8,8 +8,11 @@ def home_page(request):
         #new_item_text = request.POST['item_text'] #1
         Item.objects.create(text=request.POST['item_text'])
         #Item.objects.create(text=new_item_text) #2
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
-    items = Item.objects.all()
+    #items = Item.objects.all()
     
-    return render(request,'home.html', {'items':items})
+    return render(request,'home.html')
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html',{'items':items})
